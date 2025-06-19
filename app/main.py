1a4b7335e29ceaca87c6bc9f7a3b8ded9bf95ef7
usstatus <usstatusbot@gmail.com>
2025-06-19 10:38:56 -0400

    CORS middleware ayarlandi

--- app/main.py
+++ app/main.py
@@ -8,3 +8,15 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Immigration Bot Backend çalışıyor!"}
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://immigration-frontend-gilt.vercel.app/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
